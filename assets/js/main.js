969ef253030cbcf0beacf022d5735a8106be169b
Yonnys quintero <leafar.qr@gmail.com>
2018-09-05 13:35:15 -0400

    Aquarella-w

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// ****************************boton-up**************************
+
+function icono(){
+		document.getElementById("icono-up").classList.toggle ("fa-angle-double-down");
+	}
+	document.getElementById("icono-down").onclick = function(){
+		icono();
+	}
+// **************************** end boton-up**************************
